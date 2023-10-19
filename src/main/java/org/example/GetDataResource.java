09654f04192946e691d1848289dc8145fb34b57e
mpuss <herdiseptanto2017@gmail.com>
2023-10-19 09:42:31 +0700

    add method setWriteFile (for set save image to local dir) add method setJsonParsing (for set parse object json to String) add method setValueAmountJson (for set value-amount-json to add nameFile and loop param page)

--- src/main/java/org/example/GetDataResource.java
+++ src/main/java/org/example/GetDataResource.java
@@ -1,11 +1,13 @@
 package org.example;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 
 public class GetDataResource {
@@ -13,35 +15,70 @@ public class GetDataResource {
     private HttpURLConnection httpURLConnection;
     private BufferedReader bufferedReader;
     private StringBuilder stringBuilder;
-    private JSONObject jsonObjectObj1,jsonObjectObj2;
+    private JSONObject jsonObjectObj1, jsonObjectObj2;
     private JSONArray jsonArray;
 
-    void setApiUrl(String paramQuery, String parampAmount, String paramPageUrl) throws IOException {
-        String apiUrl = "https://unsplash.com/napi/search/photos?query="+paramQuery+"&per_page="+parampAmount+"&page="+paramPageUrl;
-        url = new URL(apiUrl);
-        httpURLConnection = (HttpURLConnection) url.openConnection();
-        httpURLConnection.setRequestMethod("GET");
-        bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
-        stringBuilder = new StringBuilder();
-        String inputLine;
-        while ((inputLine = bufferedReader.readLine()) != null) {
-            stringBuilder.append(inputLine);
+    void setApiUrl(String paramQuery, int parampAmount, String paramPageUrl) {
+        try {
+            String apiUrl = "https://unsplash.com/napi/search/photos?query=" + paramQuery + "&per_page=" + parampAmount + "&page=" + paramPageUrl;
+            url = new URL(apiUrl);
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("GET");
+            bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+            stringBuilder = new StringBuilder();
+            String inputLine;
+            while ((inputLine = bufferedReader.readLine()) != null) {
+                stringBuilder.append(inputLine);
+            }
+            bufferedReader.close();
+            httpURLConnection.disconnect();
+            setValueAmountJson(paramQuery,parampAmount);
+        } catch (IOException e) {
+            System.out.println("check your connection");
         }
-        bufferedReader.close();
-        httpURLConnection.disconnect();
-        setJsonParsing(stringBuilder,"color",0);
     }
 
-    void setIndexValue(){
-
+    private int setValueAmountJson(String paramQuery, int valueAmountJson) {
+        //use for set value param page
+        int j = 0;
+        while (j <= (valueAmountJson - 1)) {
+            setJsonParsing(paramQuery,stringBuilder, j, "full");
+            j++;
+        }
+        return 0;
     }
 
-    void setJsonParsing(StringBuilder paramStringBuilder, String paramGetStringJson, int paramIndexJson){
+    private void setWriteFile(String paramUrlImage, String paramNameImage, int paramNumberSubNameFile){
+        try {
+            url = new URL(paramUrlImage);
+            String namaFile = "/home/mpuss/Pictures/gajah/"+paramNameImage+paramNumberSubNameFile+".jpg";
+            try (InputStream in = new BufferedInputStream(url.openStream());
+                 FileOutputStream fos = new FileOutputStream(namaFile)) {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = in.read(buffer, 0, 1024)) != -1) {
+                    fos.write(buffer, 0, bytesRead);
+                }
+                System.out.println("File finish to save " + namaFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (MalformedURLException e) {
+            System.out.println("check your connection");
+        }
+    }
+
+    private void setJsonParsing(String paramNameFileQuery, StringBuilder paramStringBuilder, int paramIndexJson, String paramGetVariation) {
+        //parsing json to string
         jsonObjectObj1 = new JSONObject(paramStringBuilder.toString());
         jsonArray = jsonObjectObj1.getJSONArray("results");
         jsonObjectObj1 = jsonArray.getJSONObject(paramIndexJson);
         jsonObjectObj2 = jsonObjectObj1.getJSONObject("urls");
-        paramGetStringJson = jsonObjectObj2.getString("full");
-        System.out.println(paramGetStringJson);
+        String variation = jsonObjectObj2.getString(paramGetVariation);
+        //for getString obj2 with key : urls
+        int numberSubNameImg = paramIndexJson + 1;
+        //for set file name to add 'filename[number].jpg'
+        setWriteFile(variation,paramNameFileQuery,numberSubNameImg);
+        System.out.println(paramNameFileQuery+"Nomor " + numberSubNameImg);
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,15 +3,16 @@ package org.example;
 import java.io.IOException;
 
 public class Main {
-    static private String query, amount, numberPage;
-
+    static private String query, numberPage;
+    static private int amount;
     static private GetDataResource getDataResource = new GetDataResource();
+
     public static void main(String[] args) throws IOException {
         if (args.length == 4 && args[0].equals("-g") || args[0].equals("--get")) {
             query = String.valueOf(args[1]);
-            amount = String.valueOf(args[2]);
+            amount = Integer.valueOf(args[2]);
             numberPage = String.valueOf(args[3]);
-            getDataResource.setApiUrl(query,amount,numberPage);
+            getDataResource.setApiUrl(query, amount, numberPage);
         }
     }
 }
